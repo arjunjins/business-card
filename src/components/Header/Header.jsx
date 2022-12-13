a6ac739e1122bfb6800be178d47ec06f85c50e5b
Arjun J <arjunjins2001@gmail.com>
2022-12-13 21:33:02 +0530

    Header css final added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,9 @@ import logo from "../../assets/IMG_3486.jpg";
 import "./Header.css";
 import Body from "../Body/Body";
 import Footer from "../Footer/Footer";
+import email from "../../assets/Mail.png";
+import linkedin from "../../assets/linkedin.png";
+
 const Header = () => {
   return (
     <div className="header-container">
@@ -16,8 +19,12 @@ const Header = () => {
           arjunj.website
         </a>
         <div>
-          <button>Email</button>
-          <button>LinkedIn</button>
+          <button type="button" className="btn btn-light bton">
+            <img src={email} alt="" /> Email
+          </button>
+          <button type="button" className="btn btn-primary bton">
+            <img src={linkedin} alt="" /> LinkedIn
+          </button>
         </div>
         <Body />
         <Footer />
